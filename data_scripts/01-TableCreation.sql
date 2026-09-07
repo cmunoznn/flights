fba3fbd9028eb8ab645527fee80c2f95db2e64d0
CarlosMunoz <cmunoznn@gmail.com>
2026-09-07 16:46:05 -0300

    Creating a funtion readable by airflow

---
+++ data_scripts/01-TableCreation.sql
@@ -0,0 +1,31 @@
+
+--DATA FLIGHTS TABLE CREATION
+
+CREATE TABLE IF NOT EXISTS  flights (
+    id BIGSERIAL PRIMARY KEY,
+    fr24_id TEXT,
+    flight TEXT,
+    callsign TEXT,
+    lat DOUBLE PRECISION,
+    lon DOUBLE PRECISION,
+    track INTEGER,
+    alt INTEGER,
+    gspeed INTEGER,
+    vspeed INTEGER,
+    squawk TEXT,
+    timestamp TIMESTAMPTZ,
+    source TEXT,
+    hex TEXT,
+    type TEXT,
+    reg TEXT,
+    painted_as TEXT,
+    operating_as TEXT,
+    orig_iata TEXT,
+    orig_icao TEXT,
+    dest_iata TEXT,
+    dest_icao TEXT,
+    eta TIMESTAMPTZ
+);
+
+select * from flights
+;
--- src/main.py
+++ src/main.py
@@ -1,24 +1,55 @@
-import os
 
+"""Santiago - NewYork Flight activity"""
+
+import os
 import requests
 from dotenv import load_dotenv
+import pandas as pd
+
+def execute_pipeline():
+
+    load_dotenv()
+
+    try:
+
+        """getting the API URL and token from the environment variables"""
+
+        api_url = os.getenv("FLIGHTRADAR_API_URL")
+        api_token = os.getenv("FLIGHTRADAR_API_TOKEN")
+
+        if not api_url or not api_token:
+            raise RuntimeError("Missing FLIGHTRADAR_API_URL or FLIGHTRADAR_API_TOKEN in .env")
+
+        headers = {
+            "Authorization": f"Bearer {api_token}",
+            "Accept": "application/json",
+            "Accept-Version": "v1"
+        }
+
+        response = requests.get(api_url, headers=headers, timeout=30)
+
+        print(f"Código HTTP: {response.status_code}")
+        response.raise_for_status()
+        result = response.json()
+        print(f"Resultado: {result}")
+
+        """Taking the data from the API response and creating a DataFrame"""
+            
+        df = pd.DataFrame(result["data"])
+
+        if len(df) == 0:
+            raise RuntimeError("No results found in the API response")
+
+        print(df.iloc[0,1])  # Print the first row of the DataFrame
+
+        for columns in df.columns:
+            for rows in df.index:
+                print(f"{columns}: {df.at[rows, columns]}")
 
 
-load_dotenv()
+    except Exception as e:
+        print(f"*******ERROR*******: {e}")   
 
-api_url = os.getenv("FLIGHTRADAR_API_URL")
-api_token = os.getenv("FLIGHTRADAR_API_TOKEN")
 
-if not api_url or not api_token:
-    raise RuntimeError("Faltan FLIGHTRADAR_API_URL o FLIGHTRADAR_API_TOKEN en .env")
-
-headers = {
-    "Authorization": f"Bearer {api_token}",
-    "Accept": "application/json",
-    "Accept-Version": "v1"
-}
-
-response = requests.get(api_url, headers=headers, timeout=30)
-
-print(f"Código HTTP: {response.status_code}")
-print(response.text)
+if __name__ == "__main__":
+    execute_pipeline()
